af2faae5a084e573088df13ed0436fd51903f87c
chencm <chaoming007@163.com>
2018-01-15 10:54:42 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var path = require('path');
+var os=require("os");
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var url=require("url");
+var querystring=require("querystring");
+var fs=require("fs");
+const PORT=8081;
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use((req,res)=>{
+// 	//console.log(url.parse(req.url));
+// 	//res.redirect("/public/");
+// });
+
+app.use(function(req,res,next){
+	var s=req.hostname;
+	console.log(s);
+
+	next();
+});
+
+
+app.get(["/admin/:num/:id","/aaa"], function(req, res, next) {
+	
+
+
+    console.log("12345");
+    next();
+});
+
+
+
+
+
+
+const IP=os.networkInterfaces().en0[1].address;
+app.listen(PORT,()=>{
+	console.log("链接成功！");
+	console.log("IP地址为->",IP,"端口号->",PORT);
+});
